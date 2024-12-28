ee6f5c7101ca3c123ade623ee4582ef3a80d82f6
StudentBI32 <StudentBI32@SYN13ASS-NB-522.alabuga.local>
2024-12-28 14:32:54 +0300

    close: issue #1

--- app/models.py
+++ app/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Categories(models.Model):
+    title = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Goods(models.Model):
+    name = models.CharField(max_length=255)
+    price = models.PositiveIntegerField()
+    decs = models.CharField(max_length=255)
+    categories = models.ForeignKey(Categories, on_delete=models.CASCADE)
+    manufacturer = models.ForeignKey(Manufacturer, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.categories
